6a167cf334f9f659f26a3b620715abdc68273f7f
henstyle <henspicsaypro@gmail.com>
2026-03-20 04:15:02 +0700

    Enable Docker service in development environment

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -13,6 +13,7 @@
     pkgs.curl
   ];
   # Sets environment variables in the workspace
+  services.docker.enable = true;
   env = {};
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
